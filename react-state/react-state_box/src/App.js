07451b6e9d28c71041f43ba7ac4cce9d3c025aaf
Rouzbehan <rouzbehan.amiri@gmail.com>
2023-08-01 11:36:35 +0200

    1st

--- react-state/react-state_box/src/App.js
+++ react-state/react-state_box/src/App.js
@@ -1,18 +1,20 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
-  let isActive = false;
+  const [active, setActive] = useState(false);
 
   function handleClick() {
-    isActive = !isActive;
-    // Check that the value changes correctly.
-    console.log(isActive);
+    setActive(!active);
+    console.log(active);
   }
 
   return (
     <main>
-      <div className={`box ${isActive ? "box--active" : ""}`} />
-      <button onClick={handleClick}>Activate</button>
+      <div className={`box ${active ? "box--active" : ""}`} />
+      <button onClick={handleClick}>
+        {active ? "Deactivate" : "Activate"}
+      </button>
     </main>
   );
 }
